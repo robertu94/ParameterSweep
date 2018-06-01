40319790e05a958a5597896219b17b3b44fb491d
Robert Underwood <rr.underwood94@gmail.com>
2018-06-01 11:14:38 -0400

    Fix compiler errors with Distributions
    +   add missing include header
    +   add missing `typename`s
    +   add type aliases for generic programming

--- include/Distributions.hpp
+++ include/Distributions.hpp
@@ -1,28 +1,30 @@
-template <class RandomNumberEngine, class RandomNumberDistribution>
-std::unique_ptr<RandomNumberGenerator> make_dist(RandomNumberEngine gen, RandomNumberDistribution dist)
-{
-	return std::make_unique<Distribution<RandomNumberEngine,RandomNumberDistribution>>(gen,dist);
-}
-
-
+#include <memory>
 template <class T>
 class RandomNumberGenerator
 {
 	public:
+	using result_type = T;
 	virtual T operator ()()=0;
 	virtual ~RandomNumberGenerator()=default;
 };
 
 
 template <class RandomNumberEngine, class RandomNumberDistribution>
-class Distribution: public RandomNumberGenerator<RandomNumberDistribution::result_type>
+class Distribution: public RandomNumberGenerator<typename RandomNumberDistribution::result_type>
 {
 	RandomNumberDistribution dist;
 	RandomNumberEngine gen;
 
 	public:
+	using param_type = typename RandomNumberDistribution::param_type;
 	Distribution(RandomNumberEngine gen, RandomNumberDistribution dist): dist(dist), gen(gen) {}
 
-	RandomNumberDistribution::result_type operator()() override { return dist(gen); }
+	typename RandomNumberDistribution::result_type operator()() override { return dist(gen); }
 
 };
+
+template <class RandomNumberEngine, class RandomNumberDistribution>
+auto make_dist(RandomNumberEngine gen, RandomNumberDistribution dist)
+{
+	return std::make_unique<Distribution<RandomNumberEngine,RandomNumberDistribution>>(gen,dist);
+}
